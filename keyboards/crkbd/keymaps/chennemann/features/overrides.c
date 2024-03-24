a29bc12fc9633124c129084fbe3f623369b41c74
Christoph Hennemann <christoph.hennemann@gmail.com>
2024-03-25 00:18:03 +0100

    cleaning up keymap configuration

---
+++ keyboards/crkbd/keymaps/chennemann/features/overrides.c
@@ -0,0 +1,9 @@
+#include QMK_KEYBOARD_H
+
+const key_override_t delete_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_BSPC, KC_DEL);
+
+// This globally defines all key overrides to be used
+const key_override_t **key_overrides = (const key_override_t *[]){
+    &delete_key_override,
+    NULL // Null terminate the array of overrides!
+};
---
+++ keyboards/crkbd/keymaps/chennemann/keymap.h
@@ -0,0 +1,6 @@
+#pragma once
+#include QMK_KEYBOARD_H
+#include "layers.h"
+
+#include "keymap_german.h"
+#include "sendstring_german.h"
