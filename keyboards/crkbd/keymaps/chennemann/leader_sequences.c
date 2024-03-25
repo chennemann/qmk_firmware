b7e2fe06814ed5d97756a1074f322a7298a72cd3
Christoph Hennemann <christoph.hennemann@gmail.com>
2024-03-25 16:28:06 +0100

    Eagerly apply case-mods

--- keyboards/crkbd/keymaps/chennemann/leader_sequences.c
+++ keyboards/crkbd/keymaps/chennemann/leader_sequences.c
@@ -4,23 +4,23 @@
 
 void *leader_caps_func(uint16_t keycode) {
     switch (keycode) {
-        case DE_W:
+        case DE_Q:
             toggle_caps_word_mode(CWMODE_NORMAL);
             return NULL;
-        case DE_E:  // KONSTANT_CASE
+        case DE_W:  // KONSTANT_CASE
             toggle_caps_word_mode(CWMODE_CONSTANT_CASE);
             return NULL;
-        case DE_K:  // kebab-case
-            toggle_caps_word_space_sub(DE_MINS);
-            return NULL;
-        case DE_U:  // U for underscore ... snake_case
-            toggle_caps_word_space_sub(DE_UNDS);
-            return NULL;
-        case DE_C:  // C for camelCase
+        case DE_E:  // camelCase
             toggle_caps_word_mode(CWMODE_CAMEL_CASE);
             return NULL;
         case DE_S:  // slash/case (ex. path/to/file)
             toggle_caps_word_space_sub(DE_SLSH);
+            return NULL;
+        case DE_R:  // snake_case
+            toggle_caps_word_space_sub(DE_UNDS);
+            return NULL;
+        case DE_T:  // kebab-case
+            toggle_caps_word_space_sub(DE_MINS);
             return NULL;
     }
     return NULL;
