81074aa589a9e22669976079cea8652a2286cb66
Christoph Hennemann <christoph.hennemann@gmail.com>
2024-03-25 10:12:34 +0100

    Merge updated caps_word handling

--- keyboards/crkbd/keymaps/chennemann/features/caps_word.h
+++ keyboards/crkbd/keymaps/chennemann/features/caps_word.h
@@ -6,13 +6,15 @@ typedef enum {
     CWMODE_NUM_LOCK,
     CWMODE_ARROW_SHIFT,
     CWMODE_CONSTANT_CASE,  // CONSTANT_CASE
-    CWMODE_KEBAB_CASE,     // kebab-case
-    CWMODE_SNAKE_CASE,     // snake_case
     CWMODE_CAMEL_CASE      // camelCase
+    CWMODE_SPACE_SUB       // will substitute space with a predefined character. can be used for
+                           // kebab-case, snake_case, slash/case (path/to/file), or others.
 } caps_word_mode_t;
 
 #define CAPS_WORD_MODE_DEFAULT CWMODE_NORMAL
+#define CAPS_WORD_SPACE_SUB_DEFAULT KC_SPACE
 
 caps_word_mode_t g_caps_word_mode;
 
 bool toggle_caps_word_mode(caps_word_mode_t new_mode);
+bool toggle_caps_word_space_sub(uint16_t sub_keycode);
