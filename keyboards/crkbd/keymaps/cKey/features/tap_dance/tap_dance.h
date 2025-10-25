ece500be82fc2477b036d1db21cdae58e269adea
Christoph Hennemann <christoph.hennemann@gmail.com>
2025-10-26 01:24:51 +0200

    Introduce retroactive shift
    This capitalizes a letter even if shift was already released on key-down

--- keyboards/crkbd/keymaps/cKey/features/tap_dance/tap_dance.h
+++ keyboards/crkbd/keymaps/cKey/features/tap_dance/tap_dance.h
@@ -33,9 +33,19 @@ typedef struct {
 
 td_state_t evaluate_tap_dance_state(tap_dance_state_t *state);
 
-bool was_keycode_handled(uint16_t keycode);
-void reset_handled_keycode(void);
-void post_tap_dance_shift_task(void);
+// Special Mod Tap Handling to allow for fast typing of shift + MT keys
+bool was_mt_handled(uint16_t keycode);
+void reset_mt_handling(void);
+
+// Special Letter Handling to allow for retroactive shifting of a key
+// This is useful for fast typing
+bool is_retroactive_shift_enabled(void);
+void consume_retroactive_shift(void);
+void reset_retroactive_shift(void);
+
+// Matrix Scan Task to allow for post tap dance cleanups
+// This is useful for use-cases such as retroactively turning a SINGLE_TAP into a SINGLE_HOLD
+void tap_dance_cleanup_task(void);
 
 // Shift Tap Dances
 void x_shift_on_each_tap(tap_dance_state_t *state, void *user_data);
