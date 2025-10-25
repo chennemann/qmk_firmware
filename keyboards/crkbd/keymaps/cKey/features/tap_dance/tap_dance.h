073d20aad8863d9169fc4f30a4ff8d2b99fc32cd
Christoph Hennemann <christoph.hennemann@gmail.com>
2025-10-26 01:03:57 +0200

    Implement selection hotkeys, caps word functionality and improve shift tap dance behavior

--- keyboards/crkbd/keymaps/cKey/features/tap_dance/tap_dance.h
+++ keyboards/crkbd/keymaps/cKey/features/tap_dance/tap_dance.h
@@ -1,3 +1,5 @@
+#pragma once
+
 #include "quantum.h"
 
 // Tap Dance States
@@ -10,13 +12,24 @@ typedef enum {
     TD_DOUBLE_HOLD,
     TD_DOUBLE_SINGLE_TAP, // Send two single taps
     TD_TRIPLE_TAP,
-    TD_TRIPLE_HOLD
+    TD_TRIPLE_HOLD,
+    TD_SUSPENDED,
 } td_state_t;
+
+typedef struct {
+    uint16_t keycode;
+    uint16_t dt_keycode;  // Optional double-tap keycode
+    bool has_dt_keycode;  // Flag to check if dt_keycode is set
+    uint16_t dt_layer;    // Optional double-tap keycode
+    bool has_dt_layer;    // Flag to check if dt_keycode is set
+} tap_dance_config_t;
 
 
 typedef struct {
-    bool is_press_action;
     td_state_t state;
+    bool enforce_shift;
+    uint16_t suspend_time;
+    bool cw_mode_active;
 } td_tap_t;
 
 td_state_t evaluate_tap_dance_state(tap_dance_state_t *state);
@@ -24,10 +37,12 @@ td_state_t evaluate_tap_dance_state(tap_dance_state_t *state);
 
 
 // Shift Tap Dances
-void x_shift_finished(tap_dance_state_t *state, void *user_data, uint16_t keycode);
+void x_shift_on_each_tap(tap_dance_state_t *state, void *user_data);
+void x_shift_on_each_release(tap_dance_state_t *state, void *user_data);
+void x_shift_finished(tap_dance_state_t *state, void *user_data);
 void x_shift_reset(tap_dance_state_t *state, void *user_data);
 
 
 // Layer Tap Dances
-void x_layer_finished(tap_dance_state_t *state, void *user_data, uint16_t keycode, uint16_t layer);
-void x_layer_reset(tap_dance_state_t *state, void *user_data, uint16_t layer);
+void x_layer_finished(tap_dance_state_t *state, void *user_data, uint16_t keycode, uint16_t layer, uint16_t dt_layer);
+void x_layer_reset(tap_dance_state_t *state, void *user_data, uint16_t layer, uint16_t dt_layer);
