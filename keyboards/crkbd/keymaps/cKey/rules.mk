f3bae163b3fdedf248cffd5f62cecaf0cc03678c
Christoph Hennemann <christoph.hennemann@gmail.com>
2025-10-26 00:40:24 +0200

    Add cKey keymap based on default modified to work with a rp2040

---
+++ keyboards/crkbd/keymaps/cKey/rules.mk
@@ -0,0 +1,18 @@
+# Enable Half-Duplex Serial communication between the two halves
+SERIAL_DRIVER = vendor
+
+
+# Disable Bootmagic as the Corne has a dedicated Reset Button
+# https://docs.qmk.fm/features/bootmagic#advanced-bootmagic
+BOOTMAGIC_ENABLE = no
+
+
+# Disable OLED as it is not currently used
+# https://docs.qmk.fm/features/oled_driver
+OLED_ENABLE = no
+
+
+# Disable the RGB Lighting feature
+RGBLIGHT_ENABLE = no
+RGB_MATRIX_ENABLE = no
+
